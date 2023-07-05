e36826ddade95530f0bfb204db3408e86e6e303d
habibmy <habib.my@rediffmail.com>
2023-07-05 20:02:25 +0530

    First Working Version

---
+++ background.js
@@ -0,0 +1,34 @@
+// Replace 'your-iframe-id' with the actual ID of your iframe element
+function enableInputsInIframe(tab) {
+  chrome.scripting.executeScript({
+    target: {
+      tabId: tab.id,
+    },
+    func: () => {
+      var iframe = document.getElementById("contentIframe");
+      if (iframe) {
+        var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        var inputs = iframeDoc.getElementsByTagName("input");
+        var selects = iframeDoc.getElementsByTagName("select");
+
+        for (var i = 0; i < inputs.length; i++) {
+          inputs[i].removeAttribute("disabled");
+        }
+        for (var i = 0; i < selects.length; i++) {
+          selects[i].removeAttribute("disabled");
+        }
+      }
+    },
+  });
+}
+
+// Replace 'your-iframe-id' with the actual ID of your iframe element
+function enableInputsInCurrentTab() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length > 0) {
+      enableInputsInIframe(tabs[0]);
+    }
+  });
+}
+
+chrome.action.onClicked.addListener(enableInputsInCurrentTab);
